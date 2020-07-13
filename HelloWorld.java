aaa26658508243e96ceb167ff67de67d6fae26e2
subkumak <subkumak@gmail.com>
2020-07-14 00:16:53 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+
+                Date dateobj = new Date();
+
+                System.out.println(df.format(dateobj));
+                System.out.println("\n Hello World ");
+
+
+	}
+
+}
